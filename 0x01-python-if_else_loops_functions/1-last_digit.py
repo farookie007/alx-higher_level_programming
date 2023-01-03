e2d1ecdfd25148f4c285ae5b5926b06a2a65e6e4
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2023-01-03 23:11:29 +0000

    Initial commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+# in order to get the absolute value of number is number is -ve
 flag = -1 if number < 0 else 1
-l_digit = (flag * number) % 10
+# converts the last digit to -ve if number was originally -ve
+l_digit = ((flag * number) % 10) * flag
 print(f"Last digit of {number} is {l_digit} and is ", end="")
 if l_digit == 0:
     print(l_digit)
