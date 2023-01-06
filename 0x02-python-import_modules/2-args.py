9ab7aca48dc87f739d34d6f01a20e776bd9ad8d1
Farouk Abiodun Adegboyega <faroukadekunle001@gmail.com>
2023-01-06 20:23:46 +0000

    Initial commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import sys
 
-
     n_args = len(sys.argv) - 1
     print(f"{n_args} argument{'' if n_args == 1 else 's'}{'.' if n_args == 0 else ':'}")
     for i, arg in enumerate(sys.argv[1:], start=1):
