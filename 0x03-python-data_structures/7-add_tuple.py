7c835a160dee34058001de01ecdac42d9c78099d
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 20:58:21 +0000

    Initial commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,10 +11,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = list(tuple_b)
     if l1 < 2:
         if l1 == 0:
-            a[0] = 0
-        a[1] = 0
+            a += [0]
+        a += [0]
     if l2 < 2:
         if l2 == 0:
-            b[0] = 0
-        b[1] = 0
+            b += [0]
+        b += [0]
     return add(a[0], b[0]), add(a[1], b[1])
