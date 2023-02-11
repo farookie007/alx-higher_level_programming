5c62ac5f48e638872a9f24b36800c836e1c1083b
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 22:02:05 +0000

    Initial commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix[:]
+    new_matrix = matrix.copy()
     for i, j in enumerate(matrix):
         for x, y in enumerate(j):
             new_matrix[i][x] = y ** 2
