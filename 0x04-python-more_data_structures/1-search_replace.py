593042cbbf532eff6248fd5e200fd0aababdda13
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 22:20:48 +0000

    Initial commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return [replace if my_list[i] == search else my_list[i] for i in my_list]
+    return [replace if i == search else i for i in my_list]
