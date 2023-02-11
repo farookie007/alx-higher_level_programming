0108c503dd54982ad87035e1f2f0b6117d0c36b5
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 19:29:16 +0000

    Resolved syntax error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    return "".join([letter for letter in my_string if letter not in ['c', 'C'])
+    return "".join([letter for letter in my_string if letter not in ['c', 'C']])
