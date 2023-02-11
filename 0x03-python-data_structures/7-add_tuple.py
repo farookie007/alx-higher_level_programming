cda9def0dd7e98ba1eb455b52fd3c7d61d8e6ebf
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 20:43:52 +0000

    Resolved syntax error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -16,4 +16,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if l2 == 0:
             tuple_b[0] = 0
         tuple_b[-1] = 0
-    return add(tuple_a[0], tuple_b[0], add(tuple_a[1], tuple_b[1])
+    return add(tuple_a[0], tuple_b[0]), add(tuple_a[1], tuple_b[1])
