5edef5f103b032bd94dcabfa15483dd3afba4339
farookie007 <faroukadekunle001@gmail.com>
2023-02-11 20:16:39 +0000

    Initial commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for row in matrix:
         for i, col in enumerate(row, start=1):
             print("{:d}".format(col), end="\n" if i == len(row) else " ")
-
