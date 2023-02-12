aa084a6ec9e0cfcaad16656191f95994a0ee50d3
farookie007 <faroukadekunle001@gmail.com>
2023-02-12 18:39:55 +0000

    Initial commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        a_dictionary = dict()
     sorted_keys = sorted(list(a_dictionary.keys()))
     for k in sorted_keys:
         print("{}: {}".format(k, a_dictionary.get(k)))
