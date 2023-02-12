caa17471ff6fdf9530a58873f947facead2ed929
farookie007 <faroukadekunle001@gmail.com>
2023-02-12 19:30:02 +0000

    Resolved Runtime Error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,6 +3,7 @@
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    for k, v in a_dictionary.items():
-        if v == value:
+    keys = a_dictionary.keys()
+    for k in keys:
+        if a_dictionary.get(k) == value:
             a_dictionary.pop(k)
