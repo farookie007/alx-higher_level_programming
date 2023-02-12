253391297b560b48a50cc9ffd0b14c9a28cee43b
farookie007 <faroukadekunle001@gmail.com>
2023-02-12 19:31:42 +0000

    Converted the sequence of the keys into list

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    keys = a_dictionary.keys()
+    keys = list(a_dictionary.keys())
     for k in keys:
         if a_dictionary.get(k) == value:
             a_dictionary.pop(k)
