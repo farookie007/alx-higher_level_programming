f98926017fa3e40a8d6cf5ba823e81deebc9af46
farookie007 <faroukadekunle001@gmail.com>
2023-02-13 21:54:10 +0000

    Resolved syntax error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     no = 0
-        for element in my_list[:x]:
-            try:
-                print("{}".format(element), end="")
-                no += 1
-            except:
-                break
-            finally:
-                print()
-                return no
+    for element in my_list[:x]:
+        try:
+            print("{}".format(element), end="")
+            no += 1
+        except:
+            break
+        finally:
+            print()
+            return no
