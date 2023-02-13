295aa83a0835fe9363fd05d601a3ef5beaa5ce86
farookie007 <faroukadekunle001@gmail.com>
2023-02-13 23:36:24 +0000

    Refactored the code

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            print(result)
-            return result
+            pass
+    return result
