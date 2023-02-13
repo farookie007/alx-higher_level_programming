868bce80073b834c3081a8c1e0f6da87b920f8c0
farookie007 <faroukadekunle001@gmail.com>
2023-02-13 22:19:48 +0000

    Added exception raise for `IndexError` exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     no = 0
-    for i in my_list[:x]:
+    for i in range(x):
         try:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(my_list[i]), end="")
             no += 1
-        except (ValueError):
+        except (ValueError, TypeError):
             continue
+        except IndexError:
+            raise
     print()
     return no
