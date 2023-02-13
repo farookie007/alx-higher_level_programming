98b9a26c5e338f8ee8e8a7ff01092e4f44cc2aa3
farookie007 <faroukadekunle001@gmail.com>
2023-02-13 21:57:04 +0000

    Restructured the return statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
             no += 1
         except:
             break
-        finally:
-            print()
-            return no
+    print()
+    return no
