efbcf3593739a4c3f8e187935902de5bd68c43c8
farookie007 <faroukadekunle001@gmail.com>
2023-02-13 22:24:12 +0000

    Initial commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def div(a, b):
+    return a / b
+
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = div(a, b)
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
