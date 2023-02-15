45f62d0a30ad2750e750663b6f4541f92a3490f1
farookie007 <faroukadekunle001@gmail.com>
2023-02-15 23:53:37 +0000

    Initial commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,13 +35,12 @@ class Square:
     @position.setter
     def position(self, value: tuple):
         """Sets the value of the position attr of the class"""
-        if all((isinstance(value, tuple),
-                all(map(lambda x: isinstance(x, int), value)),
-                len(value) == 2)):
-            if (value[0] >= 0) and (value[1] >= 0):
-                self.__position = value
-        else:
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Calculates and return the area of the `Square` object"""
