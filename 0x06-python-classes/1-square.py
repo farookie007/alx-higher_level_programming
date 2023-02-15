e7d958b237768d020cf80d55aab91a7f09cbcfb0
farookie007 <faroukadekunle001@gmail.com>
2023-02-15 21:37:28 +0000

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class `Square` that defines a square"""
+
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size):
+        """The constructor method for the `Square` class"""
+        self.__size = size
