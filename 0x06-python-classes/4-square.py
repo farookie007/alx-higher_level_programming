bce750c374779116c93ceb80bff37262a5d6c4b2
farookie007 <faroukadekunle001@gmail.com>
2023-02-15 21:56:54 +0000

    Initial commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contains a class `Square` that defines a square"""
+
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size=0):
+        """The constructor method for the `Square` class
+        Args:
+            size (int): the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.__size
+
+    @size.setter(self, size):
+        """Sets the value of the size of the `Square`"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates and return the area of the `Square` object"""
+        return self.size ** 2
